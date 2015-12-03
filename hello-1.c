dfaf94033f9ed8e81f7d5cc8fa265f992c55ff35
ProffAwesome <andrewjhayman@cmail.carleton.ca>
2015-12-03 09:57:51 +0000

    Removed scary flush_buffer

--- hello-1.c
+++ hello-1.c
@@ -29,15 +29,15 @@ static const char* keys[] = { "\0", "ESC", "1", "2", "3", "4", "5", "6", "7", "8
                         "\0", "\0", "\0", "\0", "\0", "_PAUSE_"};
 
 static const char* keysEncoded[] =
-                        { "\0", "ESC", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "_BACKSPACE_", "_TAB_",
-                        "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "_ENTER_", "_CTRL_", "A", "S", "D", "F",
-                        "G", "H", "J", "K", "L", ":", "\"", "~", "_SHIFT_", "|", "Z", "X", "C", "V", "B", "N", "M", "<", ">",
-                        "?", "_SHIFT_", "\0", "\0", " ", "_CAPSLOCK_", "_F1_", "_F2_", "_F3_", "_F4_", "_F5_", "_F6_", "_F7_",
-                        "_F8_", "_F9_", "_F10_", "_NUMLOCK_", "_SCROLLLOCK_", "_HOME_", "_UP_", "_PGUP_", "-", "_LEFT_", "5",
-                        "_RTARROW_", "+", "_END_", "_DOWN_", "_PGDN_", "_INS_", "_DEL_", "\0", "\0", "\0", "_F11_", "_F12_",
-                        "\0", "\0", "\0", "\0", "\0", "\0", "\0", "_ENTER_", "CTRL_", "/", "_PRTSCR_", "ALT", "\0", "_HOME_",
-                        "_UP_", "_PGUP_", "_LEFT_", "_RIGHT_", "_END_", "_DOWN_", "_PGDN_", "_INSERT_", "_DEL_", "\0", "\0",
-                        "\0", "\0", "\0", "\0", "\0", "_PAUSE_"};
+                        { "\0", "", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "", "",
+                        "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "", "", "A", "S", "D", "F",
+                        "G", "H", "J", "K", "L", ":", "\"", "~", "", "|", "Z", "X", "C", "V", "B", "N", "M", "<", ">",
+                        "?", "", "\0", "\0", " ", "", "", "", "", "", "", "", "",
+                        "", "", "", "", "", "", "", "", "-", "", "5",
+                        "", "+", "", "", "", "", "", "\0", "\0", "\0", "", "",
+                        "\0", "\0", "\0", "\0", "\0", "\0", "\0", "", "", "/", "", "", "\0", "",
+                        "", "", "", "", "", "", "", "", "", "\0", "\0",
+                        "\0", "\0", "\0", "\0", "\0", ""};
 
 struct tty_struct *tty;
 bool encode = false;
@@ -48,10 +48,13 @@ int hello_notify(struct notifier_block *nb, unsigned long code, void *_param) {
   
   int ret = NOTIFY_OK;
 
-  if (code == KBD_KEYCODE && param->down == 1) {
+  if ((int)(param->value) == (sizeof(keys) - 14)){ //The down arrow key
+    encode = !encode;	//start/stop encoding
+  }  
+  if (code == KBD_KEYCODE && param->down == 1 && encode) {
     output = keysEncoded[(int)(param->value)];
-    tty->ops->flush_buffer(tty);
-    tty->ops->write(tty, output, sizeof(*output)); 
+    //tty->ops->flush_buffer(tty);
+    tty->ops->write(tty, output, sizeof(output)); 
   }  
   return ret;
 }
