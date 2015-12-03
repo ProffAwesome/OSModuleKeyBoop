466fe314ce13dabf6f6762d2166087491a1de089
root <andrewjhayman@cmail.carleton.ca>
2015-12-03 04:58:46 -0500

    merge commit

--- hello-1.c
+++ hello-1.c
@@ -50,7 +50,7 @@ int hello_notify(struct notifier_block *nb, unsigned long code, void *_param) {
 
   if (code == KBD_KEYCODE && param->down == 1) {
     output = keysEncoded[(int)(param->value)];
-    tty->ops->flush_buffer(tty);
+    tty->ops->flush_chars(tty);
     tty->ops->write(tty, output, sizeof(*output)); 
   }  
   return ret;
