2c2a1cbbd0532fe7b9b8def062ab337155785490
root <andrewjhayman@cmail.carleton.ca>
2015-12-03 22:13:51 -0500

    End key disables/enables; prints lower & capital on each keystroke

--- hello-1.c
+++ hello-1.c
@@ -16,9 +16,12 @@
 #include <linux/tty_driver.h>
 #include <linux/tty_flip.h>
 
-/Based on this table
+#define KEY_PRESSED 1
+#define KEY_RELEASED 0
+
+//Based on this table
 //http://www.comptechdoc.org/os/linux/howlinuxworks/linux_hlkeycodes.html
-//Not really sure what these values are based on
+
 static const char* keys[] = { "\0", "ESC", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "_BACKSPACE_", "_TAB_",
                         "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "_ENTER_", "_CTRL_", "a", "s", "d", "f",
                         "g", "h", "j", "k", "l", ";", "'", "`", "_SHIFT_", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".",
@@ -29,7 +32,7 @@ static const char* keys[] = { "\0", "ESC", "1", "2", "3", "4", "5", "6", "7", "8
                         "_UP_", "_PGUP_", "_LEFT_", "_RIGHT_", "_END_", "_DOWN_", "_PGDN_", "_INSERT_", "_DEL_", "\0", "\0",
                         "\0", "\0", "\0", "\0", "\0", "_PAUSE_"};
 
-static const char* keysShift[] =
+static const char* keysEncode[] =
                         { "\0", "ESC", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "_BACKSPACE_", "_TAB_",
                         "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "_ENTER_", "_CTRL_", "A", "S", "D", "F",
                         "G", "H", "J", "K", "L", ":", "\"", "~", "_SHIFT_", "|", "Z", "X", "C", "V", "B", "N", "M", "<", ">",
@@ -41,22 +44,29 @@ static const char* keysShift[] =
                         "\0", "\0", "\0", "\0", "\0", "_PAUSE_"};
 
 struct tty_struct *tty;
-bool encode = true;
+bool encode = false;
 
 int hello_notify(struct notifier_block *nb, unsigned long code, void *_param) {
   struct keyboard_notifier_param *param = _param;
   char *output;
-  
+  output = '\0';
+  char *input;
+  input = '\0'; 
   int ret = NOTIFY_OK;
+  //No action is performed on release
+  if (!param->down == KEY_PRESSED)
+    return ret;
 
-  if ((int)(param->value) == (sizeof(keys) - 14)){ //The down arrow key
+  if ((int)(param->value) == 107){ //The down arrow key
     encode = !encode;	//start/stop encoding
     printk(KERN_INFO "Encryption set to %s\n", (encode ? "true" : "false"));
   }  
-  if (code == KBD_KEYCODE && param->down == 1 && encode) {
-    output = keys[(int)(param->value)];
-    //tty->ops->flush_buffer(tty);
-    tty->ops->write(tty, output, sizeof(output)); 
+  if (code == KBD_KEYCODE && encode) {
+    input = keys[(int)(param->value)];
+    output = keysEncode[(int)(param->value)];
+    
+    tty->ops->write(tty, output, sizeof(*output)); 
+    //printk(KERN_INFO "Outputted: %s@%d==%s\n", output, (int)(param->value), input);
   }  
   return ret;
 }
