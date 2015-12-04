30cd1d8093155f7fd6ecccc98646c40a6cce7d9e
root <andrewjhayman@cmail.carleton.ca>
2015-12-04 00:47:11 -0500

    Removed special keys from key list

--- hello-1.c
+++ hello-1.c
@@ -22,26 +22,16 @@
 //Based on this table
 //http://www.comptechdoc.org/os/linux/howlinuxworks/linux_hlkeycodes.html
 
-static const char* keys[] = { "\0", "ESC", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "_BACKSPACE_", "_TAB_",
-                        "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "_ENTER_", "_CTRL_", "a", "s", "d", "f",
-                        "g", "h", "j", "k", "l", ";", "'", "`", "_SHIFT_", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".",
-                        "/", "_SHIFT_", "\0", "\0", " ", "_CAPSLOCK_", "_F1_", "_F2_", "_F3_", "_F4_", "_F5_", "_F6_", "_F7_",
-                        "_F8_", "_F9_", "_F10_", "_NUMLOCK_", "_SCROLLLOCK_", "_HOME_", "_UP_", "_PGUP_", "-", "_LEFT_", "5",
-                        "_RTARROW_", "+", "_END_", "_DOWN_", "_PGDN_", "_INS_", "_DEL_", "\0", "\0", "\0", "_F11_", "_F12_",
-                        "\0", "\0", "\0", "\0", "\0", "\0", "\0", "_ENTER_", "CTRL_", "/", "_PRTSCR_", "ALT", "\0", "_HOME_",
-                        "_UP_", "_PGUP_", "_LEFT_", "_RIGHT_", "_END_", "_DOWN_", "_PGDN_", "_INSERT_", "_DEL_", "\0", "\0",
-                        "\0", "\0", "\0", "\0", "\0", "_PAUSE_"};
+static const char* keys[] = { "\0", "", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "", "",
+                        "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "", "", "a", "s", "d", "f",
+                        "g", "h", "j", "k", "l", ";", "'", "`", "", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".",
+                        "/"};
 
 static const char* keysShift[] =
-                        { "\0", "ESC", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "_BACKSPACE_", "_TAB_",
-                        "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "_ENTER_", "_CTRL_", "A", "S", "D", "F",
-                        "G", "H", "J", "K", "L", ":", "\"", "~", "_SHIFT_", "|", "Z", "X", "C", "V", "B", "N", "M", "<", ">",
-                        "?", "_SHIFT_", "\0", "\0", " ", "_CAPSLOCK_", "_F1_", "_F2_", "_F3_", "_F4_", "_F5_", "_F6_", "_F7_",
-                        "_F8_", "_F9_", "_F10_", "_NUMLOCK_", "_SCROLLLOCK_", "_HOME_", "_UP_", "_PGUP_", "-", "_LEFT_", "5",
-                        "_RTARROW_", "+", "_END_", "_DOWN_", "_PGDN_", "_INS_", "_DEL_", "\0", "\0", "\0", "_F11_", "_F12_",
-                        "\0", "\0", "\0", "\0", "\0", "\0", "\0", "_ENTER_", "CTRL_", "/", "_PRTSCR_", "ALT", "\0", "_HOME_",
-                        "_UP_", "_PGUP_", "_LEFT_", "_RIGHT_", "_END_", "_DOWN_", "_PGDN_", "_INSERT_", "_DEL_", "\0", "\0",
-                        "\0", "\0", "\0", "\0", "\0", "_PAUSE_"};
+                        { "\0", "", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "", "",
+                        "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "", "", "A", "S", "D", "F",
+                        "G", "H", "J", "K", "L", ":", "\"", "~", "", "|", "Z", "X", "C", "V", "B", "N", "M", "<", ">",
+                        "?"};
 
 struct tty_struct *tty;
 bool encode = false;
@@ -60,13 +50,15 @@ int hello_notify(struct notifier_block *nb, unsigned long code, void *_param) {
     encode = !encode;	//start/stop encoding
     printk(KERN_INFO "Encryption set to %s\n", (encode ? "true" : "false"));
   }  
-  if (code == KBD_KEYCODE && encode) {
+
+  //If input is a key, encode setting is enabled, and its a letter
+  if (code == KBD_KEYCODE && encode && (int)(param->value) < 54) {
     if (param->shift == KEY_PRESSED)
       output = keysShift[(int)(param->value)];
     else
       output = keys[(int)(param->value)];
     tty->ops->write(tty, output, sizeof(*output)); 
-    //printk(KERN_INFO "Buffer: %d==%s\n", (int)(param->value), input);
+    printk(KERN_INFO "Buffer: %d==%s\n", (int)(param->value), input);
   }  
   return ret;
 }
